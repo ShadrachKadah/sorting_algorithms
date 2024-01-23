4d6e16716cc2cbad1972b505c0a7ec3ff3888b8e
ShadrachKadah <shadrachaudukadah1@gmail.com>
2024-01-23 11:57:29 -0800

    7. Merge sort

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,6 +1,5 @@
 #include "sort.h"
 
-
 void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **current);
 void swap_node_behind(listint_t **list, listint_t **tail, listint_t **current);
 void cocktail_sort_list(listint_t **list);
